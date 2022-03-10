3ad64679516ce1389318183eaf9d49c9731a9330
HenriquePSBrito <jhowhp@yahoo.com.br>
2022-03-10 10:03:13 -0300

    Implementando CRUD no ClienteController

--- log-api/src/main/java/com/algaworks/logapi/controller/ClienteController.java
+++ log-api/src/main/java/com/algaworks/logapi/controller/ClienteController.java
@@ -1,24 +1,82 @@
 package com.algaworks.logapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.logapi.domain.model.Cliente;
 import com.algaworks.logapi.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes") //tira a obrigatoriedade da chamada do GetMapping("/clientes")
 public class ClienteController {
 	
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> Listar(){
 		return clienteRepository.findAll();
 	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+		/*alternativa ao codigo acima --->
+		 * Depois do (@PathVariable Long clienteId) {
+		 * return clienteRepository.findById(clienteId).map(Rcliente -> ResponseEntity.ok(cliente)).orElse(ResponseEntity.notFound().build());
+		 * }*/ 
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	//void = corpo da resppsta nao vai existir
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		
+		if(!clienteRepository.existsById(clienteId)) { //senao existir o id, retornar 404
+			return ResponseEntity.notFound().build(); 
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build(); //como nao tem corpo de resposta, vai retornar 204 "sem conteudo"
+	}
+	
+	
 }
