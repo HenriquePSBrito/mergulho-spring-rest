f8d5de4d267ba5b0b77644f306eace00e7cddcc6
HenriquePSBrito <jhowhp@yahoo.com.br>
2022-03-10 08:46:20 -0300

    Usando Jakarta Persistence (JPA)

--- log-api/src/main/java/com/algaworks/logapi/controller/ClienteController.java
+++ log-api/src/main/java/com/algaworks/logapi/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.logapi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,13 @@ import com.algaworks.logapi.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Misa");
-		cliente1.setTelefone("11 99999-0001");
-		cliente1.setEmail("misa@email.com");
-		
-		var cliente2 = new Cliente(); //var = novo metodo para declarar variavel
-		cliente2.setId(2L);
-		cliente2.setNome("Kyra");
-		cliente2.setTelefone("11 99999-0002");
-		cliente2.setEmail("kyra@email.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+	public List<Cliente> Listar(){
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
+	
 
 }
--- log-api/src/main/java/com/algaworks/logapi/domain/model/Cliente.java
+++ log-api/src/main/java/com/algaworks/logapi/domain/model/Cliente.java
@@ -1,12 +1,43 @@
 package com.algaworks.logapi.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	//@Column(name = "nome") -- nao precisa especificar pq é o mesmo nome na base
 	private String nome;
 	private String email;
+	@Column(name = "fone")
 	private String telefone;
 	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
 	public Cliente() {
 	}
 	
@@ -45,7 +76,5 @@ public class Cliente {
 	}
 	
 	
-	
-
 
 }
