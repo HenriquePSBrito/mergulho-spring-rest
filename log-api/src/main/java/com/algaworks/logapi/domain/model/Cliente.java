91065da8a106e7cf98c31ba19a59033fb76bc5d6
HenriquePSBrito <jhowhp@yahoo.com.br>
2022-03-10 10:18:07 -0300

    Validando com Bean Validation

--- log-api/src/main/java/com/algaworks/logapi/domain/model/Cliente.java
+++ log-api/src/main/java/com/algaworks/logapi/domain/model/Cliente.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente {
@@ -14,13 +16,21 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	//@Column(name = "nome") -- nao precisa especificar pq é o mesmo nome na base
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Size(max = 255)
 	private String email;
+	
 	@Column(name = "fone")
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
-	
-	
+		
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
